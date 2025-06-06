e41ca7b60276297493df54b9fed15039f06a3ceb
NIXONHS <nixon2000paul@gmail.com>
2025-06-05 22:08:31 -0500

    Cambiar estilo visual si no hay cupos en un curso corregido

--- frontend/src/views/admin/AdminEvents.jsx
+++ frontend/src/views/admin/AdminEvents.jsx
@@ -311,9 +311,9 @@ const AdminEvents = () => {
                   <div className={`detail-item ${eve.cupo_dis_eve === 0 ? 'cupos-agotados-admin' : 'cupos-disponibles-admin'}`}>
                     <UserCheck size={16} className="icon-inline" />
                     <span>
-                      <strong>Cupos disponibles:</strong> {eve.cupo_dis_eve === 0 
+                      {eve.cupo_dis_eve === 0 
                         ? `🚫 AGOTADO (0 de ${eve.cupo_max_eve})` 
-                        : `${eve.cupo_dis_eve || 0} de ${eve.cupo_max_eve}`
+                        : `Cupos disponibles: ${eve.cupo_dis_eve || 0} de ${eve.cupo_max_eve}`
                       }
                     </span>
                   </div>
